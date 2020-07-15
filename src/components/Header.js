1426df70d15115913e605f4edff242ef3b7b006b
Ayesha116 <ayeshajawed1168@gmail.com>
2020-07-15 23:22:55 +0500

    ready to submit, still not responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: "red",
   },
   link: {
     marginRight: theme.spacing(5),
